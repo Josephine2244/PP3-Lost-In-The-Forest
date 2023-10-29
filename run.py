fcdba08caeebba4fda110ba2ccba16f6c02b7aa3
Josephine2244 <>
2023-10-29 13:11:12 +0000

    Add docstring to top of run.py, add game title

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+"""
+This is the main game play. The functions bring the players
+from scene to scene depending on their choices along the way.
+"""
 from art import *
 from termcolor import colored
 from storytext import *
+
+tprint("Lost In The Forest", font="big")
