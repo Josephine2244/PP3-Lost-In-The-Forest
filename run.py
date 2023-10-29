27687c53d4a766b4c5abe8a2c30289765a0ed515
Josephine2244 <>
2023-10-29 13:15:28 +0000

    Add start game story to storytext.py, add start game function docstring in run.py

--- run.py
+++ run.py
@@ -7,3 +7,10 @@ from termcolor import colored
 from storytext import *
 
 tprint("Lost In The Forest", font="big")
+
+
+def start_game():
+    """
+    This sets up the game for the player.
+    They enter their name to personalise the experience.
+    """
---
+++ storytext.py
@@ -0,0 +1,10 @@
+"""
+Contains all the story segments for the game.
+"""
+directions = ["left", "right", "cross", "go back", "jump over",
+              "speak", "fight", "hit troll", "give cloak", "fight",
+              "investigate", "explore", "yes", "no"]
+
+START_GAME_STORY = """Welcome to the Forest.
+The aim of the game is to find your way out.
+Each choice you make will decide the challenges you will face."""
